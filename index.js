bdecc64485bd88212008642111480bcb514e2863
William <wabarringer@gmail.com>
2023-01-27 12:20:15 -0800

    Psuedocode

---
+++ index.js
@@ -0,0 +1,11 @@
+// TODO: Present user with a landing page with a link to a notes page
+
+// TODO: When link is selected present user with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+
+// TODO: When user enters a new note title and the note’s text a Save icon appears in the navigation at the top of the page
+
+// TODO: When user clicks on the Save icon the new note they entered is saved and appears in the left-hand column with the other existing notes
+
+// TODO: When user clicks on an existing note in the list in the left-hand column that note appears in the right-hand column
+
+// TODO: When user clicks on the Write icon in the navigation at the top of the page they are presented with empty fields to enter a new note title and the note’s text in the right-hand column
