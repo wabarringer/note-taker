db13774bfcb4f1dd13362260973a56775fad443c
William <wabarringer@gmail.com>
2023-01-27 16:54:25 -0800

    Created routes

--- index.js
+++ index.js
@@ -14,33 +14,60 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/index.html"));
-});
-
-// TODO: `GET /notes` should return the `notes.html` file.
-app.get("/notes", (req, res) => {
-  fs.readFile("./notes.json", "utf-8", (err, data) => {
+// TODO: `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
     if (err) {
       res.status(500).send("Server error");
       throw err;
     } else {
-      const notesData = JSOON.parse(data);
+      const notesData = JSON.parse(data);
       res.json(notesData);
-      return "./public/assets/notes.html";
     }
   });
 });
-// TODO: `GET *` should return the `index.html` file.
-// TODO: `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
+
 // TODO: `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. * You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+app.post("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    if (err) {
+      res.status(500).send("Server error");
+      throw err;
+    } else {
+      const notesData = JSON.parse(data);
+      req.body.id = uuidv4();
+      notesData.push(req.body);
+      fs.writeFile(
+        "./db/db.json",
+        JSON.stringify(notesData, null, 4),
+        (err) => {
+          if (err) {
+            res.status(500).send("oh no!");
+            throw err;
+          } else {
+            res.json(req.body);
+          }
+        }
+      );
+    }
+  });
+});
+
+// TODO: `GET /notes` should return the `notes.html` file.
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+// TODO: `GET *` should return the `index.html` file. * is a catch-all
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.listen(3000, function () {
   console.log("Listening on port 3000");
